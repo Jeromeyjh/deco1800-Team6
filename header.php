e1f87f921121850e37cac5a1e0d345944763862e
Olivialeighton <71741631+Olivialeighton@users.noreply.github.com>
2020-09-24 20:35:02 +1000

    Add files via upload

---
+++ header.php
@@ -0,0 +1,28 @@
+<?php $activePage = basename($_SERVER['SCRIPT_FILENAME'], '.php'); ?>
+<!doctype html>
+<html lang="en">
+
+	<head>
+
+		<meta charset="utf-8">
+		<title>header</title>
+		<link rel="stylesheet" href="style.css">
+
+	</head>
+
+	<body>
+
+		<header>
+			Catch Me If You Can!
+		</header>
+
+		<nav>
+			<ul>
+				<li>
+					<a href="index.php">Quiz</a>
+				</li>
+				<li>
+					<a href="contact.php">Research</a>
+				</li>
+			</ul>
+		</nav>
